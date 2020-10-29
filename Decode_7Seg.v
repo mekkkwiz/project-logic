7f61c76cb9eff86525faac988016d342bd0f3600
mekkkwiz <59276758+mekkkwiz@users.noreply.github.com>
2020-10-29 22:51:07 +0700

    bord1

--- Decode_7Seg.v
+++ Decode_7Seg.v
@@ -1,20 +1,18 @@
  module Decode_7Seg(
      F, E, D, C, B, A,
      leda, ledb, ledc, ledd, lede, ledf, ledg,
-     leda_2, ledb_2, ledc_2, ledd_2, lede_2, ledf_2, ledg_2
  );
 	input F, E, D, C, B, A;
   	output leda, ledb, ledc, ledd, lede, ledf, ledg;
-    output leda_2, ledb_2, ledc_2, ledd_2, lede_2, ledf_2, ledg_2;
 
   	reg [6:0] seg_data_1;
   	reg [6:0] seg_data_2;
   	reg [5:0] DataIn;
 
 	always @(A or B or C or D or E or F) begin
- 	DatIn = { F, E, D, C, B, A };
+ 	DataIn = { F, E, D, C, B, A };
  	end
-    always @ (DaaIn)
+    always @ (DataIn)
     if (DataIn) begin
         case (DataIn)
             6'b000000: seg_data_1 = 7'b1111110; //0
@@ -28,139 +26,11 @@
             6'b001000: seg_data_1 = 7'b1111111; //8
             6'b001001: seg_data_1 = 7'b1111011; //9
 
-            6'b001010: seg_data_1 = 7'b1111110; //0
-            6'b001011: seg_data_1 = 7'b0110000; //1
-            6'b001100: seg_data_1 = 7'b1101101; //2
-            6'b001101: seg_data_1 = 7'b1111001; //3
-            6'b001110: seg_data_1 = 7'b0110011; //4
-            6'b001111: seg_data_1 = 7'b1011011; //5
-            6'b010000: seg_data_1 = 7'b1011111; //6
-            6'b010001: seg_data_1 = 7'b1110000; //7
-            6'b010010: seg_data_1 = 7'b1111111; //8
-            6'b010011: seg_data_1 = 7'b1111011; //9
-
-            6'b010100: seg_data_1 = 7'b1111110; //0
-            6'b010101: seg_data_1 = 7'b0110000; //1
-            6'b010110: seg_data_1 = 7'b1101101; //2
-            6'b010111: seg_data_1 = 7'b1111001; //3
-            6'b011000: seg_data_1 = 7'b0110011; //4
-            6'b011001: seg_data_1 = 7'b1011011; //5
-            6'b011010: seg_data_1 = 7'b1011111; //6
-            6'b011011: seg_data_1 = 7'b1110000; //7
-            6'b011100: seg_data_1 = 7'b1111111; //8
-            6'b011101: seg_data_1 = 7'b1111011; //9
-
-            6'b011110: seg_data_1 = 7'b1111110; //0
-            6'b011111: seg_data_1 = 7'b0110000; //1
-            6'b100000: seg_data_1 = 7'b1101101; //2
-            6'b100001: seg_data_1 = 7'b1111001; //3
-            6'b100010: seg_data_1 = 7'b0110011; //4
-            6'b100011: seg_data_1 = 7'b1011011; //5
-            6'b100100: seg_data_1 = 7'b1011111; //6
-            6'b100101: seg_data_1 = 7'b1110000; //7
-            6'b100110: seg_data_1 = 7'b1111111; //8
-            6'b100111: seg_data_1 = 7'b1111011; //9
-
-            6'b101000: seg_data_1 = 7'b1111110; //0
-            6'b101001: seg_data_1 = 7'b0110000; //1
-            6'b101010: seg_data_1 = 7'b1101101; //2
-            6'b101011: seg_data_1 = 7'b1111001; //3
-            6'b101100: seg_data_1 = 7'b0110011; //4
-            6'b101101: seg_data_1 = 7'b1011011; //5
-            6'b101110: seg_data_1 = 7'b1011111; //6
-            6'b101111: seg_data_1 = 7'b1110000; //7
-            6'b110000: seg_data_1 = 7'b1111111; //8
-            6'b110001: seg_data_1 = 7'b1111011; //9
-
-            6'b110010: seg_data_1 = 7'b1111110; //0
-            6'b110011: seg_data_1 = 7'b0110000; //1
-            6'b110100: seg_data_1 = 7'b1101101; //2
-            6'b110101: seg_data_1 = 7'b1111001; //3
-            6'b110110: seg_data_1 = 7'b0110011; //4
-            6'b110111: seg_data_1 = 7'b1011011; //5
-            6'b111000: seg_data_1 = 7'b1011111; //6
-            6'b111001: seg_data_1 = 7'b1110000; //7
-            6'b111010: seg_data_1 = 7'b1111111; //8
-            6'b111011: seg_data_1 = 7'b1111011; //9
-
-            6'b111100: seg_data_1 = 7'b1111011; //0
-            6'b111101: seg_data_1 = 7'b1111011; //1
-            6'b111110: seg_data_1 = 7'b1111011; //2
-            6'b111111: seg_data_1 = 7'b1111001; //3
         endcase
-        case (DataIn)
-            6'b000000: seg_data_2 = 7'b1111110; //0
-            6'b000001: seg_data_2 = 7'b1111110; //0
-            6'b000010: seg_data_2 = 7'b1111110; //0
-            6'b000011: seg_data_2 = 7'b1111110; //0
-            6'b000100: seg_data_2 = 7'b1111110; //0
-            6'b000101: seg_data_2 = 7'b1111110; //0
-            6'b000110: seg_data_2 = 7'b1111110; //0
-            6'b000111: seg_data_2 = 7'b1111110; //0
-            6'b001000: seg_data_2 = 7'b1111110; //0
-            6'b001001: seg_data_2 = 7'b1111110; //0
-
-            6'b001010: seg_data_2 = 7'b0110000; //1
-            6'b001011: seg_data_2 = 7'b0110000; //1
-            6'b001100: seg_data_2 = 7'b0110000; //1
-            6'b001101: seg_data_2 = 7'b0110000; //1
-            6'b001110: seg_data_2 = 7'b0110000; //1
-            6'b001111: seg_data_2 = 7'b0110000; //1
-            6'b010000: seg_data_2 = 7'b0110000; //1
-            6'b010001: seg_data_2 = 7'b0110000; //1
-            6'b010010: seg_data_2 = 7'b0110000; //1
-            6'b010011: seg_data_2 = 7'b0110000; //1
-
-            6'b010100: seg_data_2 = 7'b1101101; //2
-            6'b010101: seg_data_2 = 7'b1101101; //2
-            6'b010110: seg_data_2 = 7'b1101101; //2
-            6'b010111: seg_data_2 = 7'b1101101; //2
-            6'b011000: seg_data_2 = 7'b1101101; //2
-            6'b011001: seg_data_2 = 7'b1101101; //2
-            6'b011010: seg_data_2 = 7'b1101101; //2
-            6'b011011: seg_data_2 = 7'b1101101; //2
-            6'b011100: seg_data_2 = 7'b1101101; //2
-            6'b011101: seg_data_2 = 7'b1101101; //2
-
-            6'b011110: seg_data_2 = 7'b1111001; //3
-            6'b011111: seg_data_2 = 7'b1111001; //3
-            6'b100000: seg_data_2 = 7'b1111001; //3
-            6'b100001: seg_data_2 = 7'b1111001; //3
-            6'b100010: seg_data_2 = 7'b1111001; //3
-            6'b100011: seg_data_2 = 7'b1111001; //3
-            6'b100100: seg_data_2 = 7'b1111001; //3
-            6'b100101: seg_data_2 = 7'b1111001; //3
-            6'b100110: seg_data_2 = 7'b1111001; //3
-            6'b100111: seg_data_2 = 7'b1111001; //3
-
-            6'b101000: seg_data_2 = 7'b0110011; //4
-            6'b101001: seg_data_2 = 7'b0110011; //4
-            6'b101010: seg_data_2 = 7'b0110011; //4
-            6'b101011: seg_data_2 = 7'b0110011; //4
-            6'b101100: seg_data_2 = 7'b0110011; //4
-            6'b101101: seg_data_2 = 7'b0110011; //4
-            6'b101110: seg_data_2 = 7'b0110011; //4
-            6'b101111: seg_data_2 = 7'b0110011; //4
-            6'b110000: seg_data_2 = 7'b0110011; //4
-            6'b110001: seg_data_2 = 7'b0110011; //4
-
-            6'b110010: seg_data_2 = 7'b1011011; //5
-            6'b110011: seg_data_2 = 7'b1011011; //5
-            6'b110100: seg_data_2 = 7'b1011011; //5
-            6'b110101: seg_data_2 = 7'b1011011; //5
-            6'b110110: seg_data_2 = 7'b1011011; //5
-            6'b110111: seg_data_2 = 7'b1011011; //5
-            6'b111000: seg_data_2 = 7'b1011011; //5
-            6'b111001: seg_data_2 = 7'b1011011; //5
-            6'b111010: seg_data_2 = 7'b1011011; //5
-            6'b111011: seg_data_2 = 7'b1011011; //5
-
-            6'b111100: seg_data_2 = 7'b1011111; //6
-            6'b111101: seg_data_2 = 7'b1011111; //6
-            6'b111110: seg_data_2 = 7'b1011111; //6
-            6'b111111: seg_data_2 = 7'b1011111; //6
-        endcase
+    end else begin
+        seg_data_1 = 7'b1111111;
     end
+
 
      assign leda = ~seg_data_1[6];// if Active Low(Using ~)
      assign ledb = ~seg_data_1[5];
@@ -169,13 +39,5 @@
      assign lede = ~seg_data_1[2];
      assign ledf = ~seg_data_1[1];
      assign ledg = ~seg_data_1[0];
-
-     assign leda_2 = ~seg_data_2[6];// if Active Low(Usin
-     assign ledb_2 = ~seg_data_2[5];
-     assign ledc_2 = ~seg_data_2[4];
-     assign ledd_2 = ~seg_data_2[3];
-     assign lede_2 = ~seg_data_2[2];
-     assign ledf_2 = ~seg_data_2[1];
-     assign ledg_2 = ~seg_data_2[0];
 
  endmodule
--- PROJECT_LOGIC.v
+++ PROJECT_LOGIC.v
@@ -1,58 +1,41 @@
 module PROJECT_LOGIC(
-    button1button1, clk, reset, //input
+    button1, button2, button3, clk, reset, //input
     leda, ledb, ledc, ledd, lede, ledf, ledg,	//output to 7SEGMENT
     leda_2, ledb_2, ledc_2, ledd_2, lede_2, ledf_2, ledg_2
 );
 
-    input clk, reset, button1;
-
+    input clk, reset, button1, button2, button3;
 
     output leda, ledb, ledc, ledd, lede, ledf, ledg; //for 7segment 1st digit
     output leda_2, ledb_2, ledc_2, ledd_2, lede_2, ledf_2, ledg_2; //for 7segment 2nd digjt
 
-    output led1, led2, led3, led4, led5, led6, led7, led8;
-	wire and1_out,clk_out,clk_out1,couter_out,notBT;
-	wire button_out;
 
-    reg [2:0]todmux1;
+	wire and1_out,clk_out,clk_out1,couter_out;
+	wire button_out1, button_out2, button_out3;
 
-    wire D_1,C_1,B_1,A_1;
-    wire D_2,C_2,B_2,A_2;
+    wire F_1,E_1,D_1,C_1,B_1,A_1;
+    wire F_2,E_2,D_2,C_2,B_2,A_2;
+
 
     reg [5:0]led7sg_1;
 	reg [5:0]led7sg_2;
 
-    reg line1_trigger;
-    reg [5:0] line1_sel;
-    wire temp_1;
 
-	assign o_diff1 = diff1;
-	assign o_diff2 = diff2;
-	assign o_diff3 = diff3;
-	assign o_diff4 = diff4;
-	assign o_diff5 = diff5;
-	assign o_diff6 = diff6;
-	assign o_diff7 = diff7;
 
     freq_div fd0(clk_out,clk);//slow signal-clock speed
     freq_div fd1(clk_out1,clk);//slow signal-clock speed
 
-    counter_6bit line1_selGenerator(line1_sel,clk_out,reset);
 
-   	LightManager lightManager(line1_trigger, line1_sel);
-
-    assign  temp_1 = (line1_trigger && clk_out);
-
-    counter_3bit lightline1(todmux1,temp_1, reset);
-
-	Debouncing db(button_out,!button1,clk_out1);
-
-    assign and1_out = ((todmux1[0]) && (todmux1[1]) && (todmux1[2]) && button_out);
+	Debouncing db(button_out1,!button1,clk_out1);
+	Debouncing db2(button_out2,!button2,clk_out1);
+	Debouncing db3(button_out3,!button3,clk_out1);
 
 
-	dmux3to8 demux1(todmux1, clk, reset, led1, led2, led3, led4, led5, led6, led7);
+    //assign and1_out = ((todmux1[0]) && (todmux1[1]) && (todmux1[2]) && button_out);
+    //assign and1_out = ((todmux1[0]) && (todmux1[1]) && (todmux1[2]) && button_out);
+    //assign and1_out = ((todmux1[0]) && (todmux1[1]) && (todmux1[2]) && button_out);
 
-    counter_6bit scorecouter(led7sg_1,and1_out,reset); //should use 4-bit counter
+    counter_6bit scorecouter(led7sg_1,and1_out,reset); //should use 6-bit counter for score count
 
     assign F_1 = led7sg_1[5];
     assign E_1 = led7sg_1[4];
@@ -61,7 +44,6 @@ module PROJECT_LOGIC(
     assign B_1 = led7sg_1[1];
     assign A_1 = led7sg_1[0];
 
-	assign led8 = button1;
 
     Decode_7Seg decoder1(F_1, E_1, D_1, C_1, B_1, A_1,
 	leda, ledb, ledc, ledd, lede, ledf, ledg,
